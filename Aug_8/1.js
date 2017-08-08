09c2e423864d9643ff5ab97c9d8286c7f24574c1
godning3 <736397792@qq.com>
2017-08-08 15:08:26 +0800

    f

---
+++ Aug_8/1.js
@@ -0,0 +1,36 @@
+//分解质因数 90 : 2335
+
+function zhi(x) {
+    for(let j = 2; j < x; j++) {
+        if(x % j == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+function cut(x) {
+    if(x == 2){
+        return "2";
+    }
+    if(x < 2){
+        return "";
+    }
+    for (let i = x; i > 1; i--) {
+        if(zhi(i) && x % i == 0){
+            return cut(x/i)+i;
+        }
+    }
+}
+
+let ans = function (x) {
+    console.log(cut(x));
+}
+
+ans(90);
+ans(9090);
+ans(2);
+ans(3);
+
+//start :   2:20
+//end   :   2:32
