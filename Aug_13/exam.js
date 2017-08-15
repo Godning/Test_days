2e77f0f7dd0ec564842e696a9ac1c1dd68f49d67
godning3 <736397792@qq.com>
2017-08-15 11:52:53 +0800

    update aug_13

--- Aug_13/exam.js
+++ Aug_13/exam.js
@@ -1,3 +1,22 @@
 /**
  * Created by zhaoningli on 2017/8/15.
  */
+
+// 给定一个未排序的整数数组，找到其中位数。
+function get_mid_num(arr){
+    arr.sort();
+    if(arr.length % 2 == 0){
+        let num1 = arr[Math.floor(arr.length/2)];
+        let num2 = arr[Math.floor(arr.length/2)-1];
+        return (num1 + num2) /2;
+    }else{
+        return arr[Math.floor(arr.length/2)];
+    }
+}
+
+let arr = [[1,2,3,4,5,4],[1,2,3,4],[1],[1,2,3]];
+for (var i = 0; i < arr.length; i++) {
+    var obj = arr[i];
+    console.log(get_mid_num(obj));
+}
+
